fc8eb71041ec68868a4ae146ce5f8003b0a42d26
KashifCh_eth <103598065+KashifCh-eth@users.noreply.github.com>
2023-04-08 18:12:17 +0500

    Create V1.sol

---
+++ contracts/UUPS/V1.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier:  MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract upgradeTest is UUPSUpgradeable, OwnableUpgradeable {
+    uint public value;
+
+    function initialize(uint val) external {
+        value = val;
+    }
+
+    // This Only Implement When You Deploy Uups Upgradable Contract
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal virtual override onlyOwner {}
+}
